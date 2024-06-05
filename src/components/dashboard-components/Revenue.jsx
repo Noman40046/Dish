32e610b0eae2801a5e96d40d7fd69db21a6d2c3d
touhidul-islam-html <ti360109@gmail.com>
2024-06-05 18:36:43 +0600

    counter

--- src/components/dashboard-components/Revenue.jsx
+++ src/components/dashboard-components/Revenue.jsx
@@ -1,17 +1,12 @@
-import CountUp from 'react-countup';
+import SimpleCountUp from "./Counter";
 
 const Revenue = () => {
   return (
     <div>
       <div className="heading font-bold text-3xl">Your Total Revenue</div>
-      <CountUp start={9} end={57000}>
-        {({ countUpRef }) => (
-           <div className="revenue-ammount font-bold text-3xl bg-gradient-to-r from-indigo-600 via-red-500 to-orange-400 inline-block text-transparent bg-clip-text">
-           ৳ <span ref={countUpRef}></span>
-         </div>
-        )}
-      </CountUp>
-     
+      <div className="revenue-amount font-bold text-3xl bg-gradient-to-r from-indigo-600 via-red-500 to-orange-400 inline-block text-transparent bg-clip-text">
+        <SimpleCountUp end={57900} />
+      </div>
     </div>
   );
 };
