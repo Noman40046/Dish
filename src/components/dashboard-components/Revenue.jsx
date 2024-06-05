61182ff6ede376ed7fd6ccd732aa4c8660e6b16b
touhidul-islam-html <ti360109@gmail.com>
2024-06-05 18:00:47 +0600

    counter added

--- src/components/dashboard-components/Revenue.jsx
+++ src/components/dashboard-components/Revenue.jsx
@@ -1,15 +1,19 @@
+import CountUp from 'react-countup';
 
 const Revenue = () => {
-    return (
-        <div>
-            <div className="heading font-bold text-3xl">
-               Your Total Revenue
-            </div>
-            <div className="revenue-ammount font-bold text-3xl bg-gradient-to-r from-indigo-600 via-red-500 to-orange-400 inline-block text-transparent bg-clip-text">
-            ৳57000
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="heading font-bold text-3xl">Your Total Revenue</div>
+      <CountUp start={9} end={57000}>
+        {({ countUpRef }) => (
+           <div className="revenue-ammount font-bold text-3xl bg-gradient-to-r from-indigo-600 via-red-500 to-orange-400 inline-block text-transparent bg-clip-text">
+           ৳ <span ref={countUpRef}></span>
+         </div>
+        )}
+      </CountUp>
+     
+    </div>
+  );
 };
 
 export default Revenue;
--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import UserLoad from "../components/sendmessege/UserLoad";
+import {SendHorizontal } from "lucide-react";
 
 const Message = () => {
   return (
@@ -16,8 +17,8 @@ const Message = () => {
             className="h-32 block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
             placeholder="Your message..."
           ></textarea>
-          <div className="btn bg-black hover:bg-[#313131] text-white px-20 text-left">
-            <span>Submit</span>
+          <div className="btn bg-black hover:bg-[#313131] text-white text-left">
+            <span className="flex gap-1 justify-center items-center"><SendHorizontal size={18}/>Send</span>
           </div>
         </div>
       </form>
