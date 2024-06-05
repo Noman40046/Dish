ec16106c780dbc7795fdd7bb6d6f63350291ca3a
touhidul-islam-html <ti360109@gmail.com>
2024-06-05 17:06:17 +0600

    profile

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -5,18 +5,18 @@ import UserLoad from "../components/sendmessege/UserLoad";
 const Message = () => {
   return (
     <div className="p-4 sm:ml-64">
-      <div className="ml-8 font-semibold text-lg text-black mb-2">
+      <div className="font-semibold text-lg text-black mb-2">
         Send Messege
       </div>
       <form>
-        <div className="flex gap-4 flex-col items-start px-3 py-2 rounded-lg ">
+        <div className="flex gap-4 flex-col items-start py-2 rounded-lg ">
           <textarea
             id="chat"
             rows="1"
-            className="h-32 block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+            className="h-32 block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
             placeholder="Your message..."
           ></textarea>
-          <div className="btn bg-black hover:bg-[#313131] text-white px-20 ml-4 text-left">
+          <div className="btn bg-black hover:bg-[#313131] text-white px-20 text-left">
             <span>Submit</span>
           </div>
         </div>
